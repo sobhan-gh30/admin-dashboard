43f9f80b822d34632224dc7598ec1b24c0a64240
Sobhan ghasemi <mohammadghasemi3033@gmail.com>
2024-12-01 13:55:46 +0330

    feat : completing the not page found

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import {useRoutes} from "react-router-dom";
 import appRoutes from "./routes";
 function App() {
   let router = useRoutes(appRoutes)
-  console.log(router)
-  return (
-    <>
-      {router}
-    </>
-  );
+  let isPage = router.props.match.route.path
+  console.log(isPage)
+  if (isPage === "*"){
+    return (
+        <>
+          {router}
+        </>
+    );
+  } else {
+    return (
+        <>
+          {router}
+        </>
+    );
+  }
 }
 
 export default App;
