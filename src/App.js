08ff167844fa96e2b5d69e70e73c7af88def7f5a
Sobhan ghasemi <mohammadghasemi3033@gmail.com>
2024-12-01 15:40:34 +0330

    style : responsiving the top bar menu

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import TopBar from "./components/top bar";
 function App() {
   let router = useRoutes(appRoutes)
   let isPage = router.props.match.route.path
-  console.log(isPage)
   if (isPage === "*"){
     return (
         <>
--- src/components/top bar.js
+++ src/components/top bar.js
@@ -1,37 +1,52 @@
-import { FaHeart } from "react-icons/fa";
-import { CiBellOn , CiSettings } from "react-icons/ci";
-
-import { TfiWorld } from "react-icons/tfi";
+import {FaHeart} from "react-icons/fa";
+import {CiBellOn, CiSettings} from "react-icons/ci";
+import { HiOutlinePlus } from "react-icons/hi";
+import {TfiWorld} from "react-icons/tfi";
 import {useState} from "react";
 
-export default function TopBar(){
-    let [notifications , setNotification] = useState(2)
-    let [explore , setExplore] = useState(2)
-    let [hasProfile , setHasProfile] = useState("")
+export default function TopBar() {
+    let [notifications, setNotification] = useState(2)
+    let [explore, setExplore] = useState(2)
+    let [hasProfile, setHasProfile] = useState("")
 
-    return(
-        <div className="w-full py-2 px-5  flex justify-between items-center">
+    return (
+        <div className="w-full h-20 py-2 px-5 flex justify-between items-center shadow">
             <h2 className="h-14 flex items-center gap-2 font-bold text-3xl text-gray-800">
-                <FaHeart  />
+                <FaHeart/>
                 Sobhan
             </h2>
-            <div className="flex justify-center items-center gap-5 text-gray-500">
+            <div className="hidden sm:flex justify-center items-center gap-5 text-gray-500">
                 <span>
-                    <CiBellOn className="scale-[2] inline-block  hover:text-gray-800 transition " />
-                    <span className="bg-red-500 text-white text-sm w-5 h-5 inline-flex justify-center items-center rounded-full -translate-x-2 -translate-y-3">{notifications}</span>
+                    <CiBellOn className="scale-[2] inline-block  hover:text-gray-800 transition "/>
+                    <span
+                        className="bg-red-500 text-white text-sm w-5 h-5 inline-flex justify-center items-center rounded-full -translate-x-2 -translate-y-3">
+                        {notifications
+                        }</span>
                 </span>
 
                 <span>
-                    <TfiWorld className="scale-[1.75] inline-block  hover:text-gray-800 transition " />
-                    <span className="bg-red-500 text-white text-sm w-5 h-5 inline-flex justify-center items-center rounded-full -translate-x-2 -translate-y-3">{explore}</span>
+                    <TfiWorld className="scale-[1.75] inline-block  hover:text-gray-800 transition "/>
+                    <span
+                        className="bg-red-500 text-white text-sm w-5 h-5 inline-flex justify-center items-center rounded-full -translate-x-2 -translate-y-3">
+                        {explore}
+                    </span>
                 </span>
                 <span>
-                    <CiSettings className="scale-[2.4] inline-block  hover:text-gray-800 transition " />
+                    <CiSettings className="scale-[2.4] inline-block  hover:text-gray-800 transition "/>
                 </span>
                 <div className="w-14 h-14 border rounded-full overflow-hidden">
-                    <img className="w-full" src={hasProfile?`Img/Profiles/${hasProfile}`:"Img/Profiles/defaultP.png"} alt=""/>
+                    <img className="w-full"
+                         src={hasProfile ? `Img/Profiles/${hasProfile}` : "Img/Profiles/defaultP.png"} alt=""/>
                 </div>
             </div>
+            <button
+                className="h-7 w-7
+                text-5xl text-white font-bold
+                 hover:rotate-45 transition
+                  flex justify-center items-center sm:hidden
+                   bg-gray-900 bg-opacity-50 rounded-full">
+                <HiOutlinePlus />
+            </button>
         </div>
     )
 }
